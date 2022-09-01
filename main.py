f18d7438c5bb759e71257b6596b9de9444b522db
richybai <richybaicc@gmail.com>
2022-09-01 21:13:21 +0800

    设置seed之后实验结果

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import os
 import matplotlib.pyplot as plt 
 os.environ["OMP_NUM_THREADS"] = "1"
 ms.context.set_context(mode=ms.context.PYNATIVE_MODE, device_target="CPU")
-# ms.set_seed(10000)
+ms.set_seed(2022)
 
 if __name__ == "__main__":
 
     # g = gene_random_instance(n_train)
-    # g = fig7_instance1()
-    g = fig7_instance2()
+    g = fig7_instance1()
+    # g = fig7_instance2()
+    print("number of nodes:", len(g.nodes))
+    print("number of deges:", len(g.edges))
 
     # 相当于是输入模型的数据
     qnn = gene_qaoa_layers(g, P)
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     h = Tensor(np.zeros([lstm_layers, 1, 2]).astype(np.float32))
     c = Tensor(np.zeros([lstm_layers, 1, 2]).astype(np.float32))
     meta_loss = []
-    max_cut_loss = []
+    qnn_expectation = []
     for epoch in range(epochs):
         # batch_size, seq_len, input_size
         # num_directions * num_layers, batch_size, hidden_size
@@ -40,22 +42,23 @@ if __name__ == "__main__":
 
         loss = forward_with_loss(theta, h, c)
         theta, h, c, _ = metaqaoa(theta, h, c)
-        qnn_loss = qnn(theta.reshape(1, -1))
-        print("epoch: ", epoch,"metaqaoa loss: ", loss, "max cut loss: ", qnn_loss.squeeze())
+        expectation = qnn(theta.reshape(1, -1))
+        if (epoch+1) % 100 == 0:
+            print("epoch: ", epoch+1,"metaqaoa loss: ", loss, "qnn expectation: ", expectation.squeeze())
         meta_loss.append(loss.asnumpy().squeeze())
-        max_cut_loss.append(qnn_loss.asnumpy().squeeze())
+        qnn_expectation.append(expectation.asnumpy().squeeze())
         
-    plt.plot(range(epochs), meta_loss)
+    plt.plot(range(1, epochs+1), meta_loss)
     plt.xlabel('epoch')
     plt.ylabel('meta loss')
     plt.title('meta loss')
     plt.savefig('meta loss.png')
 
     plt.clf()  
-    plt.plot(range(epochs), max_cut_loss)
+    plt.plot(range(1, epochs+1), qnn_expectation)
     plt.xlabel('epoch')
-    plt.ylabel('maxcut loss')
-    plt.title('maxcut loss')
-    plt.savefig('maxcut loss.png')
+    plt.ylabel('qnn expectation')
+    plt.title('qnn expectation')
+    plt.savefig('expectation.png')
 
-    np.save("loss.npy", [meta_loss, max_cut_loss])
+    np.save("loss.npy", [meta_loss, qnn_expectation])
