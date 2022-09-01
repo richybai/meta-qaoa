18832e2585ba02a1bca08b4c3e31c0ed762accd0
richybai <richybaicc@gmail.com>
2022-09-01 18:50:49 +0800

    theta重复利用

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from mindspore import Tensor, ops, nn, Parameter
 import mindspore as ms
 import os
+import matplotlib.pyplot as plt 
 os.environ["OMP_NUM_THREADS"] = "1"
 ms.context.set_context(mode=ms.context.PYNATIVE_MODE, device_target="CPU")
 # ms.set_seed(10000)
@@ -25,17 +26,33 @@ if __name__ == "__main__":
     forward_with_loss = ForwardWithLoss(metaqaoa, loss_fn)
     train_one_step = TrainOneStep(forward_with_loss, optimizer)
 
-   
+    theta = Tensor(np.ones([1, P, 2]).astype(np.float32))
+    h = Tensor(np.zeros([lstm_layers, 1, 2]).astype(np.float32))
+    c = Tensor(np.zeros([lstm_layers, 1, 2]).astype(np.float32))
+    meta_loss = []
+    max_cut_loss = []
     for epoch in range(epochs):
         # batch_size, seq_len, input_size
         # num_directions * num_layers, batch_size, hidden_size
-        theta = Tensor(np.ones([1, P, 2]).astype(np.float32))
-        h = Tensor(np.ones([lstm_layers, 1, 2]).astype(np.float32))
-        c = Tensor(np.ones([lstm_layers, 1, 2]).astype(np.float32))
+        
         loss = train_one_step(theta, h, c)
 
         loss = forward_with_loss(theta, h, c)
-        # theta, h, c, _ = metaqaoa(theta, h, c)
+        theta, h, c, _ = metaqaoa(theta, h, c)
         max_cut_loss = qnn(theta.reshape(1, -1))
         print("epoch: ", epoch,"metaqaoa loss: ", loss, "max cut loss: ", max_cut_loss.squeeze())
         
+    plt.plot(meta_loss)
+    plt.xlabel('epoch')
+    plt.ylabel('meta loss')
+    plt.title('meta loss')
+    plt.savefig('meta loss.png')
+
+    plt.clf()  
+    plt.plot(max_cut_loss)
+    plt.xlabel('epoch')
+    plt.ylabel('maxcut loss')
+    plt.title('maxcut loss')
+    plt.savefig('maxcut loss.png')
+
+    np.save("loss.npy", [meta_loss, max_cut_loss])
